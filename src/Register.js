fc219dbfc185fe8651f86266d630f02a4b52c2fe
polanglo <m.kozlowski@polanglo.pl>
2024-03-14 15:46:16 +0100

    14032024 dw

--- src/Register.js
+++ src/Register.js
@@ -3,10 +3,11 @@ import { useRef, useState, useEffect} from "react";
 import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import axios from './api/axios'
 
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = '/register';
 
 const Register = () => {
     const userRef = useRef();
@@ -36,6 +37,9 @@ const Register = () => {
     }, [user])
 
     useEffect(() => {
+        const result = PWD_REGEX.test(pwd);
+        console.log(result)
+        console.log(pwd)
         setValidPwd(PWD_REGEX.test(pwd));
         setValidMatch(pwd === matchPwd);
     }, [pwd, matchPwd])
@@ -44,12 +48,57 @@ const Register = () => {
         setErrMsg('');
     }, [user, pwd, matchPwd])
 
-    return (
+    const handleSubmit = async (e) =>
+    {
+        e.preventDefault();
+        // if button enabled with JS hack
+        const v1 = USER_REGEX.test(user);
+        const v2 = PWD_REGEX.test(pwd);
+        if (!v1 || !v2) {
+            setErrMsg("Invalid Entry");
+            return;
+            setSuccess(true);
+    }try{
+        const response = await axios.post(REGISTER_URL,
+            JSON.stringify( {user, pwd} ),
+            {
+                headers : { 'Content-Type': 'application/json' },
+                withCredentials : true
+            }
+            );
+            console.log(response.data);
+            console.log(response.accessToken);
+            console.log(JSON.stringify(response));
+            setSuccess(true);
 
+    }catch(err){
+        if (!err?.response)
+        {
+            setErrMsg('No Server Response');
+        }else if(err.response?.status === 409)
+        {
+            setErrMsg('Username Taken')
+        }else
+        {
+            setErrMsg('Registration Failed')
+        }
+    }
+    }
+    return (
+        <>
+        {success ?
+        (
+            <section>
+                <h1>Success!</h1>
+                <p>
+                    <a href="#">Sign In</a>
+                    </p>
+                </section>
+        ) : (
         <section>
             <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
             <h1>Register</h1>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <label>
                     Username:
                     <span className={validName ? "valid" : "hide"}>
@@ -127,7 +176,8 @@ const Register = () => {
             </Form>
 
         </section>
-
+    )}
+    </>
     )
 }
 
